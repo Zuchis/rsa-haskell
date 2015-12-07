c8e63b35a894213a50b27ff2140a5d485e4d1464
Zuchis <liquimaster@gmail.com>
2015-12-07 15:58:14 -0200

    Added some screen display

--- properKeyGenerator.hs
+++ properKeyGenerator.hs
@@ -10,6 +10,9 @@ main = do
     let nE = read (args !! 0) :: Integer
         nK = read (args !! 1) :: Integer
         keys = genKeys nE nK
+    print $ (keys !! 0) 
+    print $ "------------------------------------"
+    print $ keys !! 1
     writeFile "public.txt" $ show (fst (keys !! 0)) ++ ";" ++ show (snd (keys !! 0))
     writeFile "private.txt" $ show (fst (keys !! 1)) ++ ";" ++ show (snd (keys !! 1))
     return ()
